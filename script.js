6b697f86c635ca49c83df3978b42e210c1c5d456
FedeNZ <fedenz.dev@gmail.com>
2022-06-16 09:35:39 -0300

    added computers choice

---
+++ script.js
@@ -0,0 +1,20 @@
+function start() {
+    let player = prompt("choose your option", "rock");
+    document.getElementById("first").innerHTML = "Your option is: " + player;   
+}
+let computersChoice = 0;  
+
+function computer() {
+    let random = Math.floor(Math.random()*3+1);
+    if (random === 1) {
+    computersChoice = "rock";
+    }
+    if (random === 2) {
+        computersChoice = "paper";
+    }
+    if (random === 3) {
+        computersChoice = "scissors";
+    }
+    document.getElementById("second").innerHTML = "Computers choice is: " + computersChoice;
+}
+console.log(random, computersChoice)
